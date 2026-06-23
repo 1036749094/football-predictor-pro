1545ccda40cb117610c4de91dd314681d60eec71
1036749094 <1036749094@qq.com>
2026-06-24 05:54:51 +0800

    Update odds_api.py

--- odds_api.py
+++ odds_api.py
@@ -1,11 +1,7 @@
-import random
-
 def get_odds():
 
-    base = random.uniform(1.8, 2.4)
-
     return {
-        "home": round(base, 2),
-        "draw": round(base + 1.2, 2),
-        "away": round(base + 1.0, 2)
+        "home": 1.85,
+        "draw": 3.20,
+        "away": 3.60
     }
